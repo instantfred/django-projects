618b9a1698ed34481afd6556eca1d75dd14c9943
Freddy Ramírez <furede87@gmail.com>
2025-03-14 10:55:24 -0600

    Register Item model with Django admin site for improved accessibility

--- mysite/foodie/admin.py
+++ mysite/foodie/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+# This code registers the Item model with the Django admin site. 
+# This makes the Item model accessible via the Django admin interface.
+admin.site.register(Item)
