43ca963c8ef8538eac9d951eb9a64faf1a29de51
Freddy Ramírez <furede87@gmail.com>
2025-03-17 12:15:34 -0600

    Use namespacing for items URL

--- mysite/foodie/urls.py
+++ mysite/foodie/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "foodie"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:item_id>/", views.details, name="details"),
